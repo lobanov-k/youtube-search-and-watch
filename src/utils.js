e366dab4469335e7160947f77c197c034a2e1bb5
Kostiantyn Lobanov <lobanov.k.k@gmail.com>
2019-11-21 00:20:36 +0200

    Fix query string function

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,6 @@ const youTubeApiKey = "AIzaSyBwFE4XTwXzl2WYvaGyOXCHHplcgZNfx-I";
 export function getRequestString(queryString) {
 	return `https://www.googleapis.com/youtube/v3/
 		search?part=snippet&maxResults=10&order=relevance
-		&q=${encodeURIComponent(queryString)}&safeSearch=strict&type=video
+		&q=${encodeURIComponent(queryString)}&safeSearch=strict&type=video&videoEmbeddable=true
 		&key=${youTubeApiKey}`;
 };
