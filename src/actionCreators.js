5d3941f86136e59629a3bc18167346a0f60266a2
Konstantin Lobanov <konstantin.lobanov@ab-soft.net>
2019-11-20 21:12:46 +0200

    Add reducers and AC

--- src/actionCreators.js
+++ src/actionCreators.js
@@ -1,4 +1,7 @@
-import { REQUEST_SEARCH } from './constants';
+import fetch from 'isomorphic-fetch';
+import { REQUEST_SEARCH, REQUEST_SEARCH_SUCCESS,
+    REQUEST_SEARCH_FAILURE, RECEIVE_VIDEOS } from './constants';
+import getRequestString from './utils';
 
 export function requestSearchAction(searchString) {
     return {
@@ -9,4 +12,34 @@ export function requestSearchAction(searchString) {
     };
 }
 
+export function recieveVideos(json) {
+    return {
+        type: RECEIVE_VIDEOS,
+        payload: {
+            json
+        }
+    };
+}
+
+export function requestVideosFailure(error) {
+    return {
+        type: REQUEST_SEARCH_FAILURE,
+        payload: {
+            error
+        }
+    };
+}
+
+export function fetchSearchVideos(searchString) {
+    return (dispatch) => {
+        dispatch(requestSearchAction(searchString));
+
+        return fetch(getRequestString(searchString))
+            .then(json => {
+                dispatch(recieveVideos(json));
+            }).catch((error) => {
+                dispatch(recieveVideos(error));
+            });
+    }
+}
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,6 @@
 import {combineReducers} from 'redux';
+import { REQUEST_SEARCH, REQUEST_SEARCH_SUCCESS,
+    REQUEST_SEARCH_FAILURE, RECEIVE_VIDEOS } from '../constants';
 
 function currentVideo(state = null, action) {
     switch (action.type) {
@@ -7,15 +9,36 @@ function currentVideo(state = null, action) {
     }
 }
 
-function searchResult(state = [], action) {
+function searchResult(state = {
+    isFetching: false,
+    searchError: false,
+    items: []
+}, action) {
     switch (action.type) {
+        case REQUEST_SEARCH:
+            return Object.assign({}, state, {
+                isFetching: true
+            });
+        case RECEIVE_VIDEOS:
+            return Object.assign({}, state, {
+                isFetching: false,
+                items: action.json
+            });
+        case REQUEST_SEARCH_FAILURE:
+            return Object.assign({}, state, {
+                isFetching: false,
+                searchError: true,
+                items: action.videos
+            });
         default:
             return state;
     }
 }
 
-function searchString(state = null, action) {
+function searchString(state = '', action) {
     switch (action.type) {
+        case REQUEST_SEARCH:
+            return action.payload.searchString;
         default:
             return state;
     }
