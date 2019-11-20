2f422a42cec539a69260f980607f81dffbccec7f
Konstantin Lobanov <konstantin.lobanov@ab-soft.net>
2019-11-20 21:11:25 +0200

    function to return api call url

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,7 @@
-export const youTubeApiKey = "AIzaSyBwFE4XTwXzl2WYvaGyOXCHHplcgZNfx-I";
+const youTubeApiKey = "AIzaSyBwFE4XTwXzl2WYvaGyOXCHHplcgZNfx-I";
+export function getRequestString(queryString) {
+	return `https://www.googleapis.com/youtube/v3/
+		search?part=snippet&maxResults=10&order=relevance
+		&q=${encodeURIComponent(queryString)}&safeSearch=strict&type=video
+		&key=${youTubeApiKey}`;
+};
