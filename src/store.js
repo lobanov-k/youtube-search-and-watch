9d3dc273351c282584b6f51c3522a46393f0e190
Kostiantyn Lobanov <lobanov.k.k@gmail.com>
2019-11-21 03:59:03 +0200

    Add localStorage manipulation middleware

--- src/store.js
+++ src/store.js
@@ -1,12 +1,17 @@
 import { createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
-import thunkMiddleware from 'redux-thunk'
+import thunkMiddleware from 'redux-thunk';
+import { save, load } from "redux-localstorage-simple";
 import reducer from './reducers';
 
 const loggerMiddleware = createLogger();
+const createStoreWithMiddleware = applyMiddleware(
+	save({ states: ["history"] })
+)(createStore);
 
-const store = createStore(
+const store = createStoreWithMiddleware(
 	reducer,
+	load({ states: ["history"] }),
 	applyMiddleware(
 		thunkMiddleware,
 		loggerMiddleware
