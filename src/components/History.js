c0b4429b1f0d0aab97959531b8a5d3dd0251edcd
Kostiantyn Lobanov <lobanov.k.k@gmail.com>
2019-11-21 03:58:27 +0200

    History component

--- src/components/History.js
+++ src/components/History.js
@@ -1,10 +1,30 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { removeVideoFromHistory, setCurrentVideo } from '../actionCreators';
 
-class History extends Component {
-static propTypes = {};
+export default function() {
+    const dispatch = useDispatch();
+    let history = useSelector(state => state.history);
 
-render() {}
+    return (
+        <div className="history">
+            {history.map((item, index) => {
+                const {id: {videoId}, snippet: {thumbnails, title} } = item;
+
+                return (
+                    <div key={index} className="history__item">
+                        <p className="history__item-text"
+                            onClick={() => dispatch(setCurrentVideo(Object.assign({}, item, {isFromHistory: true})))}>
+                            {title}
+                        </p>
+                        <span 
+                            className="history__item-delete"
+                            onClick={() => dispatch(removeVideoFromHistory(index))}>
+                            X
+                        </span>
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
-
-export default History;
