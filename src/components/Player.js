872a61c5810073a1506c2e1968764c38aa29b68e
Kostiantyn Lobanov <lobanov.k.k@gmail.com>
2019-11-21 03:57:42 +0200

    Player component

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,10 +1,56 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addVideoToHistory } from '../actionCreators';
 
-class Player extends Component {
-static propTypes = {};
+export default function() {
+    const dispatch = useDispatch();
 
-render() {}
+    let videoData = useSelector(state => state.currentVideo);
+    let [player, setPlayer] = useState(null);
+
+    const onVideoReady = (event) => {
+        event.target.playVideo();
+    }
+
+    const loadVideo = () => {
+        let player = new window.YT.Player("player-iframe", {
+            videoId: videoData.id.videoId,
+            events: {
+                'onReady': onVideoReady
+            },
+            playerVars: {
+                'origin':'http://localhost:8080'
+            }
+        });
+        setPlayer(player);
+    }
+
+    useEffect(() => {
+        if (!window.YT) {
+            const tag = document.createElement('script');
+            tag.src = 'https://www.youtube.com/iframe_api';
+
+            if (videoData) {
+                window.onYouTubeIframeAPIReady = loadVideo;
+            }
+
+            const firstScriptTag = document.getElementsByTagName('script')[0];
+            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+        } else {
+            if (!videoData) return;
+
+            if (!videoData.isFromHistory) dispatch(addVideoToHistory(videoData));
+            if (!player) {
+                loadVideo();
+            } else {
+                player.loadVideoById(videoData.id.videoId, 0, 'large');
+            }
+        }
+    }, [videoData]);
+
+    return (
+        <div >
+            <div width="1280" height="720" id="player-iframe" className="player__iframe"></div>
+        </div>
+    );
 }
-
-export default Player;
