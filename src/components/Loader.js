2d3182fffb5ea7c2291c78115185f62f89ceba20
Kostiantyn Lobanov <lobanov.k.k@gmail.com>
2019-11-24 11:41:05 +0200

    refactor of loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function Loader() {
-    return <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>;
+    return <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>;
 }
